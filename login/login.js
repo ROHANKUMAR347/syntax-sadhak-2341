89fa55bd6aed357101ce9b6aed1154f562aa1e39
Sonali Burman <sonali.b.020200@gmail.com>
2024-01-18 23:15:55 +0530

    added

---
+++ login/login.js
@@ -0,0 +1,12 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Implement your login logic here
+
+    // For demonstration purposes, showing an alert as a welcome notification
+    alert("Welcome to our website, " + username + "!");
+}
+
+
+
