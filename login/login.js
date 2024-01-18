800fc7a577d1910b6e5b2404f8403081a2dce404
Sonali Burman <sonali.b.020200@gmail.com>
2024-01-19 02:41:57 +0530

    edited

--- login/login.js
+++ login/login.js
@@ -8,5 +8,27 @@ function login() {
     alert("Welcome to our website, " + username + "!");
 }
 
+function signup() {
+    var name = document.getElementById("name").value;
+    var newUsername = document.getElementById("newUsername").value;
+    var newPassword = document.getElementById("newPassword").value;
+
+    // Implement your sign-up logic here
+
+    // For demonstration purposes, showing an alert as a welcome notification
+    alert("Welcome to our website, " + name + "!");
+}
+
+function showSignup() {
+    document.getElementById("loginContainer").style.display = "none";
+    document.getElementById("signupContainer").style.display = "block";
+}
+
+function showLogin() {
+    document.getElementById("loginContainer").style.display = "block";
+    document.getElementById("signupContainer").style.display = "none";
+}
+
+
 
 
