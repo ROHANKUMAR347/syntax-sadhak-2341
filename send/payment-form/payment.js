01da44f9744e2d269d8b2def17ac4e8ed948ad2c
Sameeer-Ahmad <sam9910333@gmail.com>
2024-01-20 23:52:44 +0530

    completed

--- send/payment-form/payment.js
+++ send/payment-form/payment.js
@@ -1,5 +1,6 @@
-function validateDeliveryForm() {
+function validateDeliveryForm(event) {
     // Get the values of the input fields
+    event.preventDefault();
     var NameValue = document.getElementById("name").value;
     var emailValue = document.getElementById("email").value;
     var cardNumber= document.getElementById("cardNumber").value;
@@ -19,27 +20,43 @@ function validateDeliveryForm() {
     ) {
       alert("Please fill in all required fields.");
     } else {
-      alert("Payment  successful!");
+      openPopup();
     }
   }
 
   var btn = document.querySelector(".btn-close");
 
-btn.addEventListener("click", ()=> {
+  btn.addEventListener("click", ()=> {
 
         window.location.href = "/send/send-money/send-money.html";
 });
-var isWhiteBackground = false;
-function toggleBackgroundColor() {
-  // Toggle between white and the default background color
-  var body = document.body;
-  var form = document.getElementById("paymentForm");
-  if (isWhiteBackground) {
-      body.style.backgroundColor = ''; 
-      form.style.backgroundColor = '';// Set to default background color
-  } else {
-      body.style.backgroundColor = 'white';
-      form.style.backgroundColor = 'rgb(39,39,39)';
-  }
-  isWhiteBackground = !isWhiteBackground; // Toggle the state
+let popup=document.getElementById("popup")
+function openPopup(){
+  document.body.classList.add("blur");
+    popup.classList.add("open-popup")
 }
+function redirectToIndex() {
+
+  window.location.href = "/rohan/index.html"; 
+}
+function closePopup(){
+  document.body.classList.remove("blur");
+    popup.classList.remove("open-popup")
+    redirectToIndex()
+
+}
+
+// var isWhiteBackground = false;
+// function toggleBackgroundColor() {
+
+//   var body = document.body;
+//   var form = document.getElementById("paymentForm");
+//   if (isWhiteBackground) {
+//       body.style.backgroundColor = ''; 
+//       form.style.backgroundColor = '';
+//   } else {
+//       body.style.backgroundColor = 'white';
+//       form.style.backgroundColor = 'rgb(39,39,39)';
+//   }
+//   isWhiteBackground = !isWhiteBackground; 
+// }
