facc39a0ba81f0f7615d261d720518a5258c99ad
Sonali Burman <sonali.b.020200@gmail.com>
2024-01-21 11:37:30 +0530

    add

--- login/login.js
+++ login/login.js
@@ -1,8 +1,14 @@
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    if(username==="password" && password==="password"){
+        alert("Welcome to our website, " + "!");
+        window.location.href="google.com";
+    } else{
+        alert("Please enter valid username and password");
+    }
 
-    alert("Welcome to our website, " + username + "!");
+    
 }
 
 function signup() {
@@ -23,6 +29,8 @@ function showLogin() {
     document.getElementById("loginContainer").style.display = "block";
     document.getElementById("signupContainer").style.display = "none";
 }
+
+
 
 
 
